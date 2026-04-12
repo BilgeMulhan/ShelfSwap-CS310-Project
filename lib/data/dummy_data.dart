4177be63fb94068f534136b75bb35205590a0da5
zeynepozeren1 <zeynep.ozeren@sabanciuniv.edu>
2026-04-12 22:00:46 +0300

    error fixed and dummy data updated with network images

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -28,6 +28,19 @@ class DummyData {
       location: 'Faculty Office',
       imageUrl: 'assets/images/VintageLamp.png',
     ),
+    ListingItem(
+      title: 'Keyboard',
+      condition: 'Used',
+      location: 'North Campus',
+      imageUrl: 'https://plugable.com/cdn/shop/products/main_ori_b946b2c9-ff40-415d-8b70-1efb64e5da61.jpg?pad_color=fff&v=1645482737&width=2200',
+    ),
+    ListingItem(
+      title: 'Desk Chair',
+      condition: 'Used',
+      location: 'North Campus',
+      imageUrl: 'https://www.sikaic.com/cdn/shop/files/sikaic-office-chair-brown-mid-century-faux-leather-leather-office-chair-with-adjustable-back-brown-1155223843.jpg?v=1744255419',
+    ),
+
   ];
 
   static List<RequestItem> requests = [
